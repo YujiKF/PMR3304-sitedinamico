86b683fab796fa0f7b6c0046949db53f588e5cb0
yujikf <yujikf@usp.br>
2024-11-17 03:24:25 -0300

    Segundo commit, apenas com views funcionais e utilizando um Django form

--- post/views.py
+++ post/views.py
@@ -1,36 +1,36 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
-# Listagem de posts
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'post/post_list.html', {'posts': posts})
 
-# Detalhes de um post
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'post/post_detail.html', {'post': post})
 
-# Criação de um novo post
 def post_create(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        post = Post.objects.create(title=title, content=content)
-        return redirect('post_list')
-    return render(request, 'post/post_create.html')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'post/post_create.html', {'form': form})
 
-# Edição de um post existente
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
-        post.title = request.POST.get('title')
-        post.content = request.POST.get('content')
-        post.save()
-        return redirect('post_list')
-    return render(request, 'post/post_edit.html', {'post': post})
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post/post_edit.html', {'form': form, 'post': post})
 
-# Remoção de um post
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
